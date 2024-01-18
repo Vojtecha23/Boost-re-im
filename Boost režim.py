0822ef20b7417031c4895f3d8c665f972ee1cd8b
Adam Vojtěch <vojtecha23@spstrutnov.cz>
2024-01-18 10:42:24 +0100

    Základ

---
+++ Boost režim.py
@@ -0,0 +1,52 @@
+import sys
+
+import pygame
+pygame.init()
+
+rozliseni_okna = (800, 600)
+
+okno = pygame.display.set_mode(rozliseni_okna)
+
+pozice_micku_x = 300
+pozice_micku_y = 200
+velikost_micku = 50
+rychlost_micku_x = 0.5
+rychlost_micku_y = 0.5
+
+pozice_hrace_x = 200
+pozice_hrace_y = 100
+rychlost_hrace = 0.2
+
+while True:
+    for udalost in pygame.event.get():
+        if udalost.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    stisknute_klavesy = pygame.key.get_pressed()
+    
+    if stisknute_klavesy[pygame.K_RIGHT]:
+        pozice_hrace_x += rychlost_hrace
+    
+    pozice_micku_x += rychlost_micku_x
+    pozice_micku_y += rychlost_micku_y
+    
+    if pozice_micku_x < 0:
+        pozice_micku_x = 0
+        rychlost_micku_x *= -1
+    if pozice_micku_y < 0:
+        pozice_micku_y = 0
+        rychlost_micku_y *= -1
+    
+    if pozice_micku_x > rozliseni_okna[0] - velikost_micku:
+        pozice_micku_x = rozliseni_okna[0] - velikost_micku
+        rychlost_micku_x *= -1
+    if pozice_micku_y > rozliseni_okna[1] - velikost_micku:
+        pozice_micku_y = rozliseni_okna[1] - velikost_micku
+        rychlost_micku_y *= -1
+    
+    okno.fill((255, 255, 255))
+    
+    pygame.draw.ellipse(okno, (0, 0, 255), (pozice_micku_x, pozice_micku_y, velikost_micku, velikost_micku))
+    
+    pygame.display.update()
