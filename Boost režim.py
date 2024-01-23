5b5ebf49921e53da2bbc278fe70e9348057607d4
Adam Vojtěch <vojtecha23@spstrutnov.cz>
2024-01-23 09:32:10 +0100

    Klávesy pokračování

--- Boost režim.py
+++ Boost režim.py
@@ -27,34 +27,45 @@ pozice_hrace_x = 200
 pozice_hrace_y = 100
 rychlost_hrace = 0.2
 
-# Klávesy
-keys = pygame.key.get_pressed()
+while True:
 
-if keys[pygame.K_LEFT] and pozice_hrace_x > 0:
-        pozice_hrace_x -= rychlost_hrace
-        
-if keys[pygame.K_RIGHT] and pozice_hrace_x < šířka - velikost_hrace:
-        pozice_hrace_x += rychlost_hrace
+    for udalost in pygame.event.get():
+        if udalost.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    # Klávesy
+    keys = pygame.key.get_pressed()
 
-   
-if keys[pygame.K_SPACE] and not boosted:
-        Boostnutí = True
-        boost_counter = trvání_boostu
-# Obraz
-white = (255,255,255)
-red = (255,0,0)
-screen.fill(white)
-pygame.draw.rect(screen, red, (pozice_hrace_x, pozice_hrace_y, velikost_hrace, velikost_hrace))
-pygame.display.flip()
-
-# Úprava boostu
-
-if Boostnutí:
-    rychlost_hrace = 10
-    boost_counter -= 1
+    if keys[pygame.K_LEFT] and pozice_hrace_x > 0:
+            pozice_hrace_x -= rychlost_hrace
+            
+    if keys[pygame.K_RIGHT] and pozice_hrace_x < šířka - velikost_hrace:
+            pozice_hrace_x += rychlost_hrace
+            
+    if keys[pygame.K_UP]:
     
-    if boost_counter == 0:
-        Boostnutí = False
-        rychlost_hrace = 5
+    if  keys[pygame.K_DOWN]:
+        
+
+       
+    if keys[pygame.K_SPACE] and not Boostnutí:
+            Boostnutí = True
+            boost_counter = trvání_boostu
+    # Obraz
+    white = (255, 255, 255)
+    red = (255, 0, 0)
+    screen.fill(white)
+    pygame.draw.rect(screen, red, (pozice_hrace_x, pozice_hrace_y, velikost_hrace, velikost_hrace))
+    pygame.display.flip()
+
+    # Úprava boostu
+
+    if Boostnutí:
+        rychlost_hrace = 10
+        boost_counter -= 1
+        
+        if boost_counter == 0:
+            Boostnutí = False
+            rychlost_hrace = 5
 
 
