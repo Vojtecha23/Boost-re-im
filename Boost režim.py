e4a35c082481a89d9b80027ee0cb466ea1c347a2
Adam Vojtěch <vojtecha23@spstrutnov.cz>
2024-01-23 09:07:22 +0100

    Obraz, boost atd..

--- Boost režim.py
+++ Boost režim.py
@@ -1,52 +1,60 @@
 import sys
-
 import pygame
+
+
 pygame.init()
 
-rozliseni_okna = (800, 600)
-
+rozliseni_okna = (800,600)
+šířka = 800
+výška = 600
 okno = pygame.display.set_mode(rozliseni_okna)
+název = pygame.display.set_caption("Boost režim")
+screen = pygame.display.set_mode((rozliseni_okna))
+# Pozice, boosty apod.
 
-pozice_micku_x = 300
-pozice_micku_y = 200
-velikost_micku = 50
-rychlost_micku_x = 0.5
-rychlost_micku_y = 0.5
+Boostnutí = False 
+trvání_boostu = 200
+boost_counter = 0
 
+
+pozice_ctverecku_x = 10
+pozice_ctverecku_y = 10
+rychlost_ctverecku_x = 10 
+rychlost_ctverecku_y = 10
+
+velikost_hrace = 100
 pozice_hrace_x = 200
 pozice_hrace_y = 100
 rychlost_hrace = 0.2
 
-while True:
-    for udalost in pygame.event.get():
-        if udalost.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    
-    stisknute_klavesy = pygame.key.get_pressed()
-    
-    if stisknute_klavesy[pygame.K_RIGHT]:
+# Klávesy
+keys = pygame.key.get_pressed()
+
+if keys[pygame.K_LEFT] and pozice_hrace_x > 0:
+        pozice_hrace_x -= rychlost_hrace
+        
+if keys[pygame.K_RIGHT] and pozice_hrace_x < šířka - velikost_hrace:
         pozice_hrace_x += rychlost_hrace
+
+   
+if keys[pygame.K_SPACE] and not boosted:
+        Boostnutí = True
+        boost_counter = trvání_boostu
+# Obraz
+white = (255,255,255)
+red = (255,0,0)
+screen.fill(white)
+pygame.draw.rect(screen, red, (pozice_hrace_x, pozice_hrace_y, velikost_hrace, velikost_hrace))
+pygame.display.flip()
+
+# Úprava boostu
+
+if Boostnutí:
+    rychlost_hrace = 10
+    boost_counter -= 1
     
-    pozice_micku_x += rychlost_micku_x
-    pozice_micku_y += rychlost_micku_y
-    
-    if pozice_micku_x < 0:
-        pozice_micku_x = 0
-        rychlost_micku_x *= -1
-    if pozice_micku_y < 0:
-        pozice_micku_y = 0
-        rychlost_micku_y *= -1
-    
-    if pozice_micku_x > rozliseni_okna[0] - velikost_micku:
-        pozice_micku_x = rozliseni_okna[0] - velikost_micku
-        rychlost_micku_x *= -1
-    if pozice_micku_y > rozliseni_okna[1] - velikost_micku:
-        pozice_micku_y = rozliseni_okna[1] - velikost_micku
-        rychlost_micku_y *= -1
-    
-    okno.fill((255, 255, 255))
-    
-    pygame.draw.ellipse(okno, (0, 0, 255), (pozice_micku_x, pozice_micku_y, velikost_micku, velikost_micku))
-    
-    pygame.display.update()
+    if boost_counter == 0:
+        Boostnutí = False
+        rychlost_hrace = 5
+
+
