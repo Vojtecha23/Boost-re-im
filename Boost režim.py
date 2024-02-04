3d2c6afcfe2f0c8ce42e8654c728b586d3c0bb6f
Adam Vojtěch <vojtecha@spstrutnov.cz>
2024-02-04 16:44:26 +0100

    Předělání protože mi nešla klávesa :D

--- Boost režim.py
+++ Boost režim.py
@@ -1,10 +1,9 @@
 import sys
 import pygame
 
-
 pygame.init()
 
-rozliseni_okna = (800,600)
+rozliseni_okna = (1000, 1000)
 šířka = 800
 výška = 600
 okno = pygame.display.set_mode(rozliseni_okna)
@@ -16,41 +15,42 @@ Boostnutí = False
 trvání_boostu = 200
 boost_counter = 0
 
-
 pozice_ctverecku_x = 10
 pozice_ctverecku_y = 10
 rychlost_ctverecku_x = 10 
 rychlost_ctverecku_y = 10
 
+pozice_hrace = 10
 velikost_hrace = 100
 pozice_hrace_x = 200
 pozice_hrace_y = 100
 rychlost_hrace = 0.2
 
 while True:
-
     for udalost in pygame.event.get():
         if udalost.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    
     # Klávesy
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_LEFT] and pozice_hrace_x > 0:
-            pozice_hrace_x -= rychlost_hrace
+        pozice_hrace_x -= rychlost_hrace
             
     if keys[pygame.K_RIGHT] and pozice_hrace_x < šířka - velikost_hrace:
-            pozice_hrace_x += rychlost_hrace
-            
-    if keys[pygame.K_UP]:
+        pozice_hrace_x += rychlost_hrace
     
-    if  keys[pygame.K_DOWN]:
-        
+    if keys[pygame.K_UP] and pozice_hrace_y > 0:
+        pozice_hrace_y -= rychlost_hrace
+    
+    if keys[pygame.K_DOWN] and pozice_hrace_y < výška - velikost_hrace:
+        pozice_hrace_y += rychlost_hrace
 
-       
     if keys[pygame.K_SPACE] and not Boostnutí:
-            Boostnutí = True
-            boost_counter = trvání_boostu
+        Boostnutí = True
+        boost_counter = trvání_boostu
+
     # Obraz
     white = (255, 255, 255)
     red = (255, 0, 0)
@@ -59,7 +59,6 @@ while True:
     pygame.display.flip()
 
     # Úprava boostu
-
     if Boostnutí:
         rychlost_hrace = 10
         boost_counter -= 1
@@ -67,5 +66,3 @@ while True:
         if boost_counter == 0:
             Boostnutí = False
             rychlost_hrace = 5
-
-
